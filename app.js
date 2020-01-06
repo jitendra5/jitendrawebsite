083f6e248f0f5d9cd41e51d81da7ae5432b77d7c
Jitendra <jitendra@Jitendra.Dlink>
2020-01-06 08:36:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+var bodyParser = require('body-parser');
+var axios = require('axios');
+const path = require('path');
+var publicDir = require('path').join(__dirname,'/public');
+app.use(express.static(publicDir));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
