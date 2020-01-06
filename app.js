7d9c06be17ef3b8e68c54164caa24f29e0b6a057
Jitendra <jitendra@Jitendra.Dlink>
2020-01-06 08:59:24 +0530

    changed port no

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var express = require('express');
-var app = express();
 var request = require('request');
 var bodyParser = require('body-parser');
 var axios = require('axios');
 const path = require('path');
+var app = express();
+
 var publicDir = require('path').join(__dirname,'/public');
 app.use(express.static(publicDir));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
